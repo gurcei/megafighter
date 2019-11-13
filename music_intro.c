5c8db036ad5cbb81e05be5ecc5502786555daa9d
Gurce Isikyildiz <Gurce.Isikyildiz@resmed.com.au>
2019-11-13 16:23:17 +1100

    Got the intro part of the intro working :)

--- music_intro.c
+++ music_intro.c
@@ -6,6 +6,75 @@
 #define DITTY_V1  2
 #define DITTY2_V1 3
 
+#define ARPA_1 \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O3 + D1_32, \
+    NE + O4 + D1_32, \
+    NA + O1 + D1_16,
+
+#define ARPAs_1_4 \
+    NAs + O3 + D1_32, \
+    NF + O4 + D1_32, \
+    NAs + O3 + D1_32, \
+    NF + O4 + D1_32, \
+    NAs + O3 + D1_32, \
+    NF + O4 + D1_32, \
+    NAs + O1 + D1_16,
+
+#define ARPC_1_4 \
+    DO_CMD_AUTO_GATE_OFF(0), \
+    NC + O4 + D1_32, \
+    NG + O4 + D1_32, \
+    NC + O4 + D1_32, \
+    NG + O4 + D1_32, \
+    NC + O4 + D1_32, \
+    NG + O4 + D1_32, \
+    DO_CMD_AUTO_GATE_OFF(1), \
+    NC + O1 + D1_16, // using this low note to get around gating problems
+
+#define ARPD_1_4 \
+    DO_CMD_AUTO_GATE_OFF(0), \
+    ND + O4 + D1_32, \
+    NA + O4 + D1_32, \
+    ND + O4 + D1_32, \
+    NA + O4 + D1_32, \
+    ND + O4 + D1_32, \
+    NA + O4 + D1_32, \
+    DO_CMD_AUTO_GATE_OFF(1), \
+    ND + O1 + D1_16, // using this low note to get around gating problems
+
+#define ARPD_1_8 \
+    ND + O4 + D1_32, \
+    NA + O4 + D1_32, \
+    ND + O1 + D1_16, // bum note
+
 // voice 1
 static int v1[] =
 {
@@ -14,36 +83,50 @@ static int v1[] =
   DO_CMD_DEFINE_SUB(DITTY_V1),
     -D1_4,
 
-    DO_CMD_AUTO_GATE_OFF(0),
-    ND + O5 + D1_32,
-    NA + O5 + D1_32,
-    ND + O5 + D1_32,
-    NA + O5 + D1_32,
-    ND + O5 + D1_32,
-    NA + O5 + D1_32,
-    ND + O5 + D1_32,
-    NA + O5 + D1_32,
-    DO_CMD_AUTO_GATE_OFF(1),
+    DO_CMD_SET_ADSR(1, 9, 0, 0),
 
-    ND + O5 + D1_8,
-    ND + O5 + D1_4,
+    ARPD_1_4
+
+    ARPD_1_8
+
+    ARPD_1_4
+
     -D1_8,
-    ND + O5 + D1_4,
-    ND + O5 + D1_8,
-    ND + O5 + D1_4,
+
+    ARPD_1_4
+    ARPD_1_8
+    ARPD_1_4
 
 		// DO_CMD_NEXT_SCREEN,
-    NC + O5 + D1_4,
-    ND + O5 + D1_8,
+    ARPC_1_4
+
+    ARPD_1_8
+  DO_CMD_SUB_RETURN,
+
+
+  DO_CMD_DEFINE_SUB(DITTY2_V1),
+    -D1_4,
+    ARPAs_1_4
+    ARPC_1_4
+    -D1_8,
+
+    DO_CMD_SET_ADSR(1, 9, 7, 0),
+    ARPA_1
+    -D1_8,
   DO_CMD_SUB_RETURN,
 
 
   DO_CMD_DEFINE_SUB(CHORUS_V1),
   DO_CMD_SUB_RETURN,
 
+
   // SONG BEGINS HERE
   // ----------------
+  DO_CMD_CONTROL(C_SAWTOOTH + C_GATE),
   DO_CMD_GOSUB(DITTY_V1),
+  DO_CMD_GOSUB(DITTY_V1),
+  DO_CMD_GOSUB(DITTY_V1),
+  DO_CMD_GOSUB(DITTY2_V1),
   DO_CMD_GOSUB(CHORUS_V1),
   REPEAT_TO_BEGINNING,
 
@@ -171,11 +254,44 @@ static int v2[] =
   DRUM_PAT1x2 \
   DRUM_PAT1n2
 
+#define BASS_1_4 \
+    NC + O2 + D1_32, \
+    NB + O1 + D1_32, \
+    NAs + O1 + D1_32, \
+    NA + O1 + D1_32, \
+    NGs + O1 + D1_32, \
+    NG + O1 + D1_32, \
+    NFs + O1 + D1_16,
+
+#define BASS_1_8 \
+    NC + O2 + D1_32, \
+    NB + O1 + D1_32, \
+    NAs + O1 + D1_16,
+
+#define KICK_1_8 \
+    NC + O4 + D1_32, \
+    NB + O3 + D1_32, \
+    NAs + O3 + D1_16,
+
 // voice 3
 static int v3[] =
 {
   DO_CMD_DEFINE_SUB(DRUM_PAT1),
+    -D1_4,
     NC + O2 + D1_4,
+    -D1_4,
+    NC + O2 + D1_4,
+    -D1_4,
+    NC + O2 + D1_4,
+    -D1_4,
+    NC + O4 + D1_8,
+    NC + O2 + D1_8,
+  DO_CMD_SUB_RETURN,
+
+
+  DO_CMD_DEFINE_SUB(DRUM_PAT2),
+    -D1_8,
+    NC + O2 + D1_8,
     NC + O4 + D1_4,
 
     NC + O2 + D1_8,
@@ -186,15 +302,40 @@ static int v3[] =
     NC + O4 + D1_8,
     NC + O2 + D1_4,
     NC + O2 + D1_8,
-    NC + O4 + D1_4,
+    NC + O4 + D1_8,
+    NC + O2 + D1_8,
   DO_CMD_SUB_RETURN,
 
+  DO_CMD_DEFINE_SUB(DRUM_PAT3),
+    -D1_8,
+    NC + O2 + D1_8,
+    NC + O4 + D1_8,
+    NC + O2 + D1_8,
+    NC + O4 + D1_8,
+    NC + O2 + D1_8,
+    NC + O2 + D1_8,
+    NC + O4 + D1_4,
+
+    NC + O4 + D1_8,
+    NG + O3 + D1_8,
+    NG + O3 + D1_8,
+    NC + O3 + D1_8,
+    NC + O3 + D1_8,
+    NG + O2 + D1_8,
+    NC + O2 + D1_8,
+  DO_CMD_SUB_RETURN,
+
+  DO_CMD_SET_ADSR(1, 9, 0, 0),
 	DO_CMD_GOSUB(DRUM_PAT1),
+
+	DO_CMD_GOSUB(DRUM_PAT2),
+	DO_CMD_GOSUB(DRUM_PAT2),
+  DO_CMD_GOSUB(DRUM_PAT3),
   REPEAT_TO_BEGINNING,
   0
 };
 
 void prepare_intro_song(void)
 {
-  prepare_song(v1, v2, v3, 32, 3, 1);
+  prepare_song(v1, v2, v3, 32, 3, 0);
 }

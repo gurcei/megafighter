d5d00f60ca00d8175e8719a742e823bf922386a2
gurcei <gurce.isikyildiz@gmail.com>
2020-02-09 17:14:03 +1100

    class-ified it (note this requires you to do "pip install wxpython"

--- sf2tool.py
+++ sf2tool.py
@@ -1,6 +1,14 @@
 #!/usr/local/bin/python
 import wx
-app = wx.App()
-frame = wx.Frame(parent=None, title='Hello World')
-frame.Show()
-app.MainLoop()
+
+class MyFrame(wx.Frame):
+  def __init__(self):
+    # Had to pass in these weird arguments for super() in python2.7 (not needed in python3?)
+    # https://stackoverflow.com/questions/38963018/typeerror-super-takes-at-least-1-argument-0-given-error-is-specific-to-any
+    super(MyFrame, self).__init__(parent=None, title='SF2 Animation Tool')
+    self.Show()
+
+if __name__ == '__main__':
+  app = wx.App()
+  frame = MyFrame()
+  app.MainLoop()
